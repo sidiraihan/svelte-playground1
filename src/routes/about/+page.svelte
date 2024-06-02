631453df7b85bdd2a08fdaa4187fcee75cede370
sidiraihan <sidiraihan@gmail.com>
2024-06-02 22:02:14 +0700

    update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,18 +2,9 @@
 <svelte:head>
     <title>about</title>
   </svelte:head>
-<style>
-	p {
-		color: purple;
-		font-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
-	}
-</style>
 <script>
 	import FormSearchEmail from '$lib/FormSearchEmail.svelte';
-    let greet = 'hellow2';
 </script>
 
-
-<p>{greet} about</p>
+<p>poc sveltekit by <a href="https://github.com/sidiraihan" target="_blank">raihan</a></p>
 <FormSearchEmail placeholder="type email address and hit Go"/>  
